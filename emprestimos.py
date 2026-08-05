9590828c9275249630b5d31b27fb1077bdd513c9
João Vitor <joaodalimass@gmail.com>
2026-08-05 20:17:45 -0300

    Feat: funcionamento no caso normal (emprestimos-usarios)

--- emprestimos.py
+++ emprestimos.py
@@ -1,5 +1,20 @@
+from usuarios import buscar_usuario
+from livros import buscar_livro
+
 emprestimos = []
 
-
 def realizar_emprestimo():
-    pass
+    cod_usuario = int(input("Código do usuario: "))
+    cod_livro = int(input("Código do livro:" )
+
+    usuario = buscar_usuario(cod_usuario)
+    livro = buscar_livro(cod_livro)
+
+    emprestimos.append({
+        "usuario": usuario,
+        "livro": livro
+    })
+
+    livro ["quantidade"] -= 1
+
+    print("Emprestimo realizado")
