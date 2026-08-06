42c8ec5cc898d275441a84d4387fec2dc6d75cc8
bianca-silva-30 <iphonedabianca30@gmail.com>
2026-08-06 18:54:33 -0300

    fix: adicionando retornos booleanos em editar_livro

--- livros.py
+++ livros.py
@@ -21,20 +21,29 @@ def cadastrar_livro(acervo, codigo, titulo, autor, quantidade):
 
 # Lógica de edição
 def editar_livro(acervo, codigo, novo_titulo=None, novo_autor=None, nova_quantidade=None):
+    livro_encontrado = None
     for livro in acervo:
         if livro['codigo'] == codigo:
-            if novo_titulo is not None:
-                if novo_titulo.strip() == "":
-                    return
-                livro['titulo'] = novo_titulo.strip()
+            livro_encontrado = livro
+            break
 
-            if novo_autor is not None:
-                livro['autor'] = novo_autor.strip()
+    if not livro_encontrado:
+        return False
 
-            if nova_quantidade is not None:
-                if nova_quantidade <= 0:
-                    return
-                livro['quantidade'] = nova_quantidade
+    if novo_titulo is not None:
+        if novo_titulo.strip() == "":
+            return False
+        livro_encontrado['titulo'] = novo_titulo.strip()
+
+    if novo_autor is not None:
+        livro_encontrado['autor'] = novo_autor.strip()
+
+    if nova_quantidade is not None:
+        if nova_quantidade <= 0:
+            return False
+        livro_encontrado['quantidade'] = nova_quantidade
+
+    return True
 
 # Lógica de remoção
 def remover_livro(acervo, codigo):
