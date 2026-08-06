57b53db1ca606e67a0806109c84960f8239f47d2
viituux <joaomarques210103@gmail.com>
2026-08-05 20:10:19 -0400

    feat : registrar devoluçao - acervo

--- relatorios.py
+++ relatorios.py
@@ -10,6 +10,13 @@ def registrar_devolucao(self, id_emprestimo: int) -> str:
     if emprestimo.get('devolvido'):
         raise ValueError("ERRO: Este Exemplar ja foi devolvido anteriormente")
 
+    emprestimo['devolvido'] = True
+    id_exemplar = emprestimo['id_exemplar']
+    if id_exemplar in self.acervo:
+        self.acervo[id_exemplar]['status'] = 'disponivel'
+        return f'Sucesso: O exemplar {id_exemplar} foi devolvido'
+    else:
+        raise ValueError("ERRO: O exemplar associado a este  emprestimo nao existe no acervo ")
 
 def relatorio():
     pass
