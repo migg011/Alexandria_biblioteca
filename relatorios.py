d5db1050037c35c0325a79db21dca468de90b885
viituux <joaomarques210103@gmail.com>
2026-08-05 20:03:57 -0400

    feat : registrar devoluçao

--- relatorios.py
+++ relatorios.py
@@ -1,2 +1,15 @@
+from Alexandria_biblioteca.emprestimos import emprestimos
+
+
+def registrar_devolucao(self, id_emprestimo: int) -> str:
+    emprestimo = emprestimos[id_emprestimo]
+
+    if not emprestimo:
+        raise ValueError("emprestimo nao encontrado")
+
+    if emprestimo.get('devolvido'):
+        raise ValueError("ERRO: Este Exemplar ja foi devolvido anteriormente")
+
+
 def relatorio():
     pass
