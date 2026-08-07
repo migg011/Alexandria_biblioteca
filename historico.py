741d73d72bc15e80d4c273347b020e6520d54efb
gustafelippe <gustavo.gugu77761@gmail.com>
2026-08-06 21:01:59 -0400

    Feat: Implementei um base de dados de usuarios e emprestimos

--- historico.py
+++ historico.py
@@ -1,2 +1,11 @@
-def exibir_historico_usuario(nome_busca: str, usuarios: dict, emprestimos: dict):
-    pass
+usuarios = [
+    {"id": 1, "nome": "Gustavo Felipe", "matricula": "A2026-08"},
+    {"id": 2, "nome": "João Viituux", "matricula": "B2026-08"}
+]
+
+emprestimos = [
+    {"usuario_id": 1, "livro": "O Alquimista", "status": "Ativo"},
+    {"usuario_id": 2, "livro": "Dom Casmurro", "status": "Ativo"},
+    {"usuario_id": 1, "livro": "1984", "status": "Devolvido"},
+    {"usuario_id": 1, "livro": "Duna", "status": "Ativo"}
+]
